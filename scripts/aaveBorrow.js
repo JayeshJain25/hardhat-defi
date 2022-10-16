92bb2fe50c4a430a272302b058b43670962c9211
jayesh jain <jayeshkjain6@gmail.com>
2022-10-16 18:01:36 +0530

    updated code

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,6 +1,83 @@
-async function main() {}
+const { getNamedAccounts, ethers } = require("hardhat");
+const { getWeth, AMOUNT } = require("../scripts/getWeth");
 
-main().then(() => process.exit(0)).catch(() => {
-    console.error(error)
-    process.exit(1)
-})
+async function main() {
+  await getWeth();
+  const { deployer } = await getNamedAccounts();
+  const lendingPool = await getLendingPool(deployer);
+  console.log(`LendingPool address ${lendingPool.address}`);
+
+  //deposit!
+  const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  //approve
+  await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer);
+  console.log("Depositing...");
+  await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
+  console.log("Deposited....");
+
+  let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+    lendingPool,
+    deployer
+  );
+
+  const daiPrice = await getDaiPrice();
+  //BORROW
+  //how much we have borrowed, how much we have in collateral, how much we can borrow
+}
+
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface",
+    "0x773616E4d11A78F511299002da57A0a94577F1f4"
+  );
+  const price = (await daiEthPriceFeed.latestRoundData())[1];
+  console.log(`The DAI/ETH price is ${price.toString()}`);
+  return price;
+}
+
+async function getBorrowUserData(lendingPool, account) {
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+    await lendingPool.getUserAccountData(account);
+  console.log(`You have ${totalCollateralETH} worth of ETH deposited.`);
+  console.log(`You have ${totalDebtETH} worth of ETH borrowed.`);
+  console.log(`You can borrow ${availableBorrowsETH} worth of ETH.`);
+  return { availableBorrowsETH, totalDebtETH };
+}
+
+async function approveErc20(
+  erc20Address,
+  spenderAddress,
+  amountToSpend,
+  account
+) {
+  const erc20Token = await ethers.getContractAt(
+    "IERC20",
+    erc20Address,
+    account
+  );
+  txResponse = await erc20Token.approve(spenderAddress, amountToSpend);
+  await txResponse.wait(1);
+  console.log("Approved!");
+}
+
+async function getLendingPool(account) {
+  const lendingPoolAddressProvider = await ethers.getContractAt(
+    "ILendingPoolAddressesProvider",
+    "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+    account
+  );
+  const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool();
+  const lendingPool = await ethers.getContractAt(
+    "ILendingPool",
+    lendingPoolAddress,
+    account
+  );
+  return lendingPool;
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(() => {
+    console.error(error);
+    process.exit(1);
+  });
